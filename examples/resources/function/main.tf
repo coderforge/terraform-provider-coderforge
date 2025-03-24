795e948a20d7e0743240f97d7e3b4409d0dc1d67
davella <support@coderforge.org>
2025-03-24 19:06:47 +0000

    DEVOPS-00001: Initial commit

--- examples/resources/function/main.tf
+++ examples/resources/function/main.tf
@@ -4,7 +4,7 @@ terraform {
       source = "terraform.coderforge.org/coderforge/coderforge"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = ">= 0.1.0"
 }
 
 provider "coderforge" {
